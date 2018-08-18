ccf7d596565e52db34328d7d6cc6df6ea4b6664c
moytri <moytritopa@gmail.com>
2018-08-17 19:22:38 -0700

    configure project and set template

---
+++ techno-resource-blog/src/com/moytri/springdemo/controller/HomeController.java
@@ -0,0 +1,16 @@
+package com.moytri.springdemo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/blog")
+public class HomeController {
+	
+	@RequestMapping("/home")
+	public String home(Model model) {
+		return "home";
+	}
+
+}
